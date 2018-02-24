22dc63b3e430207ccea200bafd9a855cf1fb8d47
wuyao1994 <wuyaoiot@163.com>
2018-02-25 00:52:02 +0800

    update password

--- zheng-common/src/main/java/com/zheng/common/util/AESUtil.java
+++ zheng-common/src/main/java/com/zheng/common/util/AESUtil.java
@@ -129,7 +129,7 @@ public class AESUtil {
 
     public static void main(String[] args) {
         String[] keys = {
-                "", "123456"
+                "password", "123456"
         };
         System.out.println("key | AESEncode | AESDecode");
         for (String key : keys) {
