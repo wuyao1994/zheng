c92c24925dac177dfd955d269614b6bc82c2ef4a
wuyao1994 <wuyaoiot@163.com>
2018-04-13 00:23:57 +0800

    add spring shrio ehcache

--- zheng-upms/zheng-upms-client/src/main/java/com/zheng/upms/client/shiro/session/UpmsSession.java
+++ zheng-upms/zheng-upms-client/src/main/java/com/zheng/upms/client/shiro/session/UpmsSession.java
@@ -3,58 +3,69 @@ package com.zheng.upms.client.shiro.session;
 import org.apache.shiro.session.mgt.SimpleSession;
 
 /**
- * 重写session
- * Created by shuzheng on 2017/2/27.
+ * 重写session Created by shuzheng on 2017/2/27.
  */
 public class UpmsSession extends SimpleSession {
 
-    public static enum OnlineStatus {
-        /**
-         * 在线
-         */
-        on_line("在线"),
+	public static enum OnlineStatus {
+		/**
+		 * 在线
+		 */
+		on_line("在线"),
 
-        /**
-         * 离线
-         */
-        off_line("离线"),
+		/**
+		 * 离线
+		 */
+		off_line("离线"),
 
-        /**
-         * 强制退出
-         */
-        force_logout("强制退出");
+		/**
+		 * 强制退出
+		 */
+		force_logout("强制退出");
 
-        private final String info;
+		private final String info;
 
-        private OnlineStatus(String info) {
-            this.info = info;
-        }
 
-        public String getInfo() {
-            return info;
-        }
-    }
 
-    // 用户浏览器类型
-    private String userAgent;
+		private OnlineStatus(String info) {
+			this.info = info;
+		}
 
-    // 在线状态
-    private OnlineStatus status = OnlineStatus.off_line;
 
-    public String getUserAgent() {
-        return userAgent;
-    }
 
-    public void setUserAgent(String userAgent) {
-        this.userAgent = userAgent;
-    }
+		public String getInfo() {
+			return info;
+		}
+	}
 
-    public OnlineStatus getStatus() {
-        return status;
-    }
+	// 用户浏览器类型
+	private String			userAgent;
 
-    public void setStatus(OnlineStatus status) {
-        this.status = status;
-    }
+	// 在线状态
+	private OnlineStatus	status	= OnlineStatus.off_line;
+
+
+
+	public String getUserAgent() {
+		return userAgent;
+	}
+
+
+
+	public void setUserAgent(String userAgent) {
+		this.userAgent = userAgent;
+	}
+
+
+
+	public OnlineStatus getStatus() {
+		return status;
+	}
+
+
+
+	public void setStatus(OnlineStatus status) {
+		this.status = status;
+	}
 
 }
